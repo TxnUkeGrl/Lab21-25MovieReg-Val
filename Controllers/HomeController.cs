a56e49cbd4465930a4e84046298f4b057636d569
TxnUkeGrl <txnukegrl@gmail.com>
2021-07-05 21:37:39 -0400

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Lab21MovieRegistration.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Diagnostics;
+
+namespace Lab21MovieRegistration.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MovieRegistration()
+        {
+            var model = new Movie();
+            model.ListGenre.Add(new SelectListItem("Horror", "Horror"));
+            return View(model);
+        }
+
+        public IActionResult Results(Movie userMovie)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(userMovie);
+            }
+            else
+            {
+                return View("MovieRegistration", userMovie);
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab21MovieRegistration.Models
+{
+    public class Movie
+    {
+        [Required(ErrorMessage = "This Title Field allows only 50 characters MAX")]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        //[Required(ErrorMessage = "A Genre MUST be selected by the provided list")]
+
+        public string Genre { get; set; }
+
+        //TODO: insert year
+        //public static int currentYear = DateTime.Now.Year;
+        [Range(1880, 2021, ErrorMessage = "Only films produced BETWEEN the years of 1880-" + "may be submitted")]
+        public string Year { get; set; }
+
+        //TODO: change to list
+        public string Actors { get; set; }
+
+        //TODO: change to list
+        public string Directors { get; set; }
+        
+        public int ID { get; set; }
+
+        public List<SelectListItem> ListGenre { get; set; } = new List<SelectListItem>();
+    }
+}
